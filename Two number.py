1888f337c6164fe57687f1457a6d6cc45cc83c73
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-11-26 13:33:44 +0530

    add two

--- Two number.py
+++ Two number.py
@@ -6,10 +6,13 @@
 #
 
 
-num1 = input("Enter the first number ")
-num2 = input("Enter the second number ")
+# num1 = input("Enter the first number ")
+# num2 = input("Enter the second number ")
+#
+# # total = num1 + num2
+# total = float(num1) + float(num2)
+# print("\n")
+# print("The sum of {0} and {1} is total of {2}".format(num1, num2, total))
 
-# total = num1 + num2
-total = float(num1) + float(num2)
-print("\n")
-print("The sum of {0} and {1} is total of {2}".format(num1, num2, total))
+
+print('The sum is %.1f' % (float(input('Enter first number: ')) + float(input('Enter second number: '))))
