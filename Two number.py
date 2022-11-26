934a60426040c9351a68b941328659593630649d
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-11-26 13:03:38 +0530

    add two

--- Two number.py
+++ Two number.py
@@ -1,6 +1,15 @@
-num1 = 7
-num2 = 9
-total = num1 + num2
+# num1 = 7
+# num2 = 9
+# total = num1 + num2
+#
+# print("The sum of {0} and {1} is {2}".format(num1, num2, total))
+#
 
-print("The sum of {0} and {1} is {2}".format(num1, num2, total))
 
+num1 = input("Enter the first number ")
+num2 = input("Enter the second number ")
+
+# total = num1 + num2
+total = float(num1) + float(num2)
+print("\n")
+print("The sum of {0} and {1} is total of {2}".format(num1, num2, total))
